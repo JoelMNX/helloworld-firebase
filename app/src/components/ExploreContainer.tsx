54701d8681b93c35316f863473095dc3c9a63723
Joel <joel@mnxonline.com>
2023-10-25 14:37:38 +0100

    test2

--- app/src/components/ExploreContainer.tsx
+++ app/src/components/ExploreContainer.tsx
@@ -4,9 +4,8 @@ interface ContainerProps {
   name: string;
 }
 
-const env = process.env.REACT_APP_BACKEND_URL_BASE;
-
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+  const env = process.env.REACT_APP_BACKEND_URL_BASE;
   return (
     <div id="container">
       <strong>{env || "no hay variable de entorno"}</strong>
