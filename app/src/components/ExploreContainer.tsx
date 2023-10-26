42b283d6635e62a9c1f8b2faf89c142618a82018
Joel <joel@mnxonline.com>
2023-10-26 09:30:52 +0100

    test3

--- app/src/components/ExploreContainer.tsx
+++ app/src/components/ExploreContainer.tsx
@@ -1,14 +1,18 @@
 import "./ExploreContainer.css";
-
+import "/home/joel/helloworld-firebase/helloworld-firebase/.github/workflows/firebase-hosting-merge.yml";
+const envv = import.meta.env.VITE_API_BACKEND_URL_BASE;
+const api = process.env.REACT_APP_API_KEY;
 interface ContainerProps {
   name: string;
 }
 
+// dotenv.config();
+
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
-  const env = process.env.REACT_APP_BACKEND_URL_BASE;
   return (
     <div id="container">
-      <strong>{env || "no hay variable de entorno"}</strong>
+      <strong>{envv ? envv : "no hay variable de entorno"}</strong>
+      <strong>{api ? api : "no hay variable de api"}</strong>
       <p>
         Explore{" "}
         <a
